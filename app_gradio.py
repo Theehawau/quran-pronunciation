d56bd722301f7cdb299c7a80f2451d9ceef2e154
theehawau <hawau.olamide.th@gmail.com>
2024-12-10 06:50:05 +0000

    [add] progress bar

--- app_gradio.py
+++ app_gradio.py
@@ -7,9 +7,7 @@ import soundfile as sf
 from utils import *
 
 
-# <h4>Instructions</h4> \
-
-with gr.Blocks(title = "Fix Transcription") as demo:  
+with gr.Blocks(title = "Quran Pronunciation") as demo:  
     dummy = gr.State(value=0)
     transcription_index = gr.State(value=0)
     current_text = "<p></p>"
@@ -24,13 +22,18 @@ with gr.Blocks(title = "Fix Transcription") as demo:
                         </div> \
                     </div>')  
     
-    # with gr.Tab("Instruction") as instruction:
-    gr.Markdown('<div style="display: flex; justify-content: space-between;"> \
+    
+    instructions = gr.Markdown('<div style="display: flex; justify-content: space-between;"> \
                 <div style="flex: 1; padding: 0px 0px;"> \
                     <div align="left"> \
-                        <h4 align="right"> سجل تسجيلًا صوتيًا بقراءة النص مع الأخطاء (بالنص الأحمر). اتبع التعليمات المميزة باللون الأصفر عند وجودها </h4> \
-                        <h4 align="right"> يرجى قراءة الجملة بدون ترتيل أو قواعد تجويد . يرجى قراءة الجملة بالخطأ الموضح مرة قبل التسجيل </h4> \
-                        <p>Thanks</p> \
+                        <h4 align="right">اتبع التعليمات الاتية </h4> \
+                        <p align="right"> .ستقوم بتسجيل صوتي لجمل . الجمل تحتوي علي أخطاء (مميزة بالون الأحمر)</p> \
+                            <p align="right">  .يرجى قراءة الجملة بالخطأ الموضح مرة قبل التسجيل</p> \
+                            <p align="right">  .اتبع التعليمات المميزة باللون الأصفر </p> \
+                            <p align="right"> .يرجى قراءة الجملة بدون ترتيل أو قواعد تجويد </p> \
+                            <p align="right">  .يرجي التسجيل في مكان هادئ </p> \
+                            <p align="right">  .يمكنك الرجوع إلي العينة كمثال </p> \
+                        <p align="right">شكرا</p> \
                     </div> \
                 </div>')
     
@@ -43,7 +46,7 @@ with gr.Blocks(title = "Fix Transcription") as demo:
         with gr.Row():
             recorder_name = gr.Textbox(label="Recorder Name", placeholder="Enter your name")    
             recorder_dialect = gr.Dropdown(
-            ["saudi", "emirati", "egyptian", "iran", "tunisian", "morrocan", "algerian"], label="Native Arabic Dialect", info="Will add more dialects later!"
+            ["saudi", "emirati", "egyptian", "tunisian", "morrocan", "algerian"], label="Native Arabic Dialect", info="Will add more dialects later!"
             )
             gender = gr.Radio(
                 ["Female", "Male"], label="Gender"
@@ -52,11 +55,11 @@ with gr.Blocks(title = "Fix Transcription") as demo:
                 ["18-50","<10", ">50"], label="Age Group"
             )
         consent = gr.Checkbox(label="I agree to donate my recordings for research purposes, your identity is protected.")
-        begin_session_btn = gr.Button("Begin Recording", variant="primary")
+        begin_session_btn = gr.Button("Begin Recording", variant="primary",visible=False)
         
         
     with gr.Column(visible=False) as recording_block:
-           
+        progress = gr.Number(1, label = "X of 50 audios" ,interactive=False)
         gr.Markdown('<h3>Error Details:</h3>')
         instruction = gr.Markdown(value=current_instruction)
         gr.Markdown('<h3>Transcription:</h3>')
@@ -73,22 +76,26 @@ with gr.Blocks(title = "Fix Transcription") as demo:
         
         audio.stop_recording(save_audio, [], [dummy, get_new_audio])
         audio.clear(save_audio, [], [dummy, get_new_audio])
-        
+    
+                # .then(clear, [], [audio,get_new_audio])\
+    
     get_new_audio.click(save_transcription, [audio, audio_file_name, transcription, recorder_path],
                         [get_new_audio])\
-                .then(clear, [], [audio,get_new_audio])\
                 .then(record_new, [recorder_path,transcription_index,audio_name],
-                      outputs=[transcription,instruction,audio_name,transcription_index, audio_file_name])\
+                      outputs=[transcription,instruction,audio_name,transcription_index, audio_file_name,progress])\
                 .then(activate_button, outputs=[get_new_audio, audio])
-            
+    
+    consent.select(show_button,[], begin_session_btn)
+    
     begin_session_btn.click(begin_session,
                             inputs=[recorder_name, gender, age, recorder_dialect],
                             outputs=[dummy, recorder_path])\
-                     .then(begin_record, [recorder_path, audio_name,transcription_index],
+                    .then(clear_markdown, [], instructions)\
+                    .then(begin_record, [recorder_path, audio_name,transcription_index],
                            [recording_block, recorder_details, transcription,instruction,
-                            audio_name,transcription_index, audio_file_name])
+                            audio_name,transcription_index, audio_file_name, progress])
                                 
                                 
 if __name__ == "__main__":   
-    demo.queue()   
-    demo.launch()
+    # demo.queue()   
+    demo.launch(share=True)
